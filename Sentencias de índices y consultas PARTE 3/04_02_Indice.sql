8701009f389bcdab1609436620b3183f81e5123a
sussiniguanziroli <sussiniguanziroli@gmail.com>
2025-11-03 16:57:48 -0300

    commit con lo ultimo de l aparte 3

--- Sentencias de índices y consultas PARTE 3/04_02_Indice.sql
+++
@@ -1,6 +0,0 @@
--- Consulta con select de WHERE para probar sin indice
-SELECT * FROM PEDIDO WHERE fecha_pedido BETWEEN '2025-01-01' AND '2025-03-31';
--- Creamos el siguiente indice
-CREATE INDEX idx_pedido_fecha ON PEDIDO(fecha_pedido);
--- Ejecutamos la misma sentencia ahora con indice para ver la diferencia en tiempo
-SELECT * FROM PEDIDO WHERE fecha_pedido BETWEEN '2025-01-01' AND '2025-03-31';
--- Sentencias de índices y consultas PARTE 3/05_02_Explain.sql
+++
@@ -1,2 +0,0 @@
--- Usada para el explain de la consulta 2
-EXPLAIN SELECT * FROM PEDIDO WHERE fecha_pedido BETWEEN '2025-01-01' AND '2025-03-31';
